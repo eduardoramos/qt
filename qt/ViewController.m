2cd7107df5c2e125b5ddb640174d41c73350ce2e
Eduardo Ramos <eduardo_ramos_b@mac.com>
2016-10-24 22:28:20 -0600

    Added files.

---
+++ qt/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  qt
+//
+//  Created by Eduardo Ramos on 5/29/16.
+//  Copyright © 2016 Eduardo Ramos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "DataRetriever.h"
+
+@interface ViewController ()
+
+@property (retain, nonatomic) DataRetriever *retriever;
+
+@end
+
+@implementation ViewController
+
+- (DataRetriever*)retriever {
+    if (!_retriever) {
+        _retriever = [[DataRetriever alloc] init];
+    }
+    return _retriever;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self.retriever updateData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
