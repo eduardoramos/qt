5fcd0e56738194d6bda59acd7b9ec1c5f8c7517d
Eduardo Ramos <eduardo_ramos_b@mac.com>
2017-04-02 01:39:59 -0600

    Created PayPeriod class

---
+++ qt/PayPeriod.h
@@ -0,0 +1,32 @@
+//
+//  PayPeriod.h
+//  qt
+//
+//  Created by Eduardo on 4/1/17.
+//  Copyright © 2017 Eduardo Ramos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PayPeriod : NSObject {
+    NSInteger   summstartOffset;
+    NSInteger   summYear;
+    NSInteger   summPP;
+    NSInteger   leavyear;
+    NSDate*     startDate;
+}
+
+@property (readwrite) NSInteger summstartOffset;
+@property (readwrite) NSInteger summYear;
+@property (readwrite) NSInteger summPP;
+@property (readwrite) NSInteger leavyear;
+@property (readonly)  NSDate*   startDate;
+
+- (id) initWithSummstartOffset:(NSInteger) aSummStartOffset
+                  withSummYear:(NSInteger) aSummYear
+                    withSummPP:(NSInteger) aSummPP
+                  withLeavyear:(NSInteger) aLeavyear;
+
+-(NSDate*) startDate;
+
+@end
---
+++ qt/PayPeriod.m
@@ -0,0 +1,39 @@
+//
+//  PayPeriod.m
+//  qt
+//
+//  Created by Eduardo on 4/1/17.
+//  Copyright © 2017 Eduardo Ramos. All rights reserved.
+//
+
+#import "PayPeriod.h"
+
+@implementation PayPeriod
+
+@synthesize summstartOffset, summYear, summPP,leavyear;
+
+- (id)init {
+    self = [super init];
+    return self;
+}
+
+- (id) initWithSummstartOffset:(NSInteger) aSummStartOffset
+               withSummYear:(NSInteger) aSummYear
+               withSummPP:(NSInteger) aSummPP
+                withLeavyear:(NSInteger)aLeavyear
+{
+    self = [super init];
+    self.summstartOffset = aSummStartOffset;
+    self.summYear = aSummYear;
+    self.summPP = aSummPP;
+    self.leavyear = aLeavyear;
+    return (self);
+}
+
+-(NSDate*) startDate{
+    NSTimeInterval interval = self.summstartOffset*24*60*60;
+    NSDate* sd = [NSDate dateWithTimeIntervalSinceReferenceDate:interval];
+    return sd;
+}
+
+@end
